4349f58ae03458932eefb24fb6cfe4738ee650d2
dave long <davelongdev@gmail.com>
2025-08-06 15:09:12 -0400

    add pyscript and student project

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+secret_number = random.randint(1, 1000)
+attempts = 0
+
+while True:
+        try:
+            
+            guess = int(input("Enter your guess number between 1-1000: "))
+            attempts += 1
+
+            if guess < secret_number:
+                print("Too low! ")
+            elif guess > secret_number:
+                print("Too high! ")
+            else:
+                print("Congratulations! You guessed it in " + str (attempts) + " attempts")
+               
+                break
+        except ValueError:
+            print("Please enter a valid number.")
