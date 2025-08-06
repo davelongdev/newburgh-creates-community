5837067d0cc93f04c6ebb664c1b432a92790b56d
dave long <davelongdev@gmail.com>
2025-08-06 15:36:29 -0400

    update student python project

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import random
-secret_number = random.randint(1, 1000)
+secret_number = random.randint(1, 2)
 attempts = 0
 
 while True:
         try:
             
-            guess = int(input("Enter your guess number between 1-1000: "))
+            guess = int(input("Enter your guess number between 1-2: "))
             attempts += 1
 
             if guess < secret_number:
@@ -14,6 +14,8 @@ while True:
                 print("Too high! ")
             else:
                 print("Congratulations! You guessed it in " + str (attempts) + " attempts")
+                print('\n')
+                continue
                
                 break
         except ValueError:
